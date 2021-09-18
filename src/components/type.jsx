1d228307447abb741160d1bf0c7c9275a6b74412
sriram-ravichandran <sriramravichandran02@gmail.com>
2021-09-18 21:13:03 +0530

    preloader

--- src/components/type.jsx
+++ src/components/type.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
+import { Typewriter,} from 'react-simple-typewriter'
 
 const Type = () => {
     return(
         <div style={{fontSize:40,color:'black'}}><Typewriter
-        words={['Developer', 'Student !']}
+        words={['Developer', 'Student']}
             loop={0}
             cursor
             cursorStyle='|'
             typeSpeed={120}
-            deleteSpeed={50}
+            deleteSpeed={70}
             delaySpeed={1000}
             
 
